64cffe02a5c76cff3eaee08e4471a1a64008ef1d
Manabu Matsuzaki <matsumana@gmail.com>
2025-05-16 19:32:02 +0900

    add micrometer-registry-otlp

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.opentelemetry.instrumentation:opentelemetry-spring-boot-starter")
+    implementation(libs.micrometerRegistryOtlp)
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
